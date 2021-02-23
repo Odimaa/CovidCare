e3745c48e41e4f68bc7a7faf0881c67d881700e0
Odimaa <odimaa.horsfall@gmail.com>
2021-02-23 23:21:01 +0100

    first

---
+++ main.js
@@ -0,0 +1,14 @@
+$('.testimonials').owlCarousel({
+    loop: false,
+    margin: 20,
+    nav: false,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        800: {
+            items: 2
+        }
+    }
+}) 
